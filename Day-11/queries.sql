c249b4560286a3345f1503856660a33e6c6e4ee2
bhartisharmaa63 <109434158+bhartisharmaa63@users.noreply.github.com>
2026-01-20 00:01:04 +0530

    Add SQL queries for hospital data analysis
    Added SQL queries for hospital data analysis including unique services, staff roles, distinct months, and event combinations.

---
+++ Day-11/queries.sql
@@ -0,0 +1,23 @@
+USE hospital;
+
+### Practice Questions:
+### 1. List all unique services in the patients table.
+SELECT DISTINCT service
+FROM patients;
+### 2. Find all unique staff roles in the hospital.
+SELECT DISTINCT role
+FROM staff;
+### 3. Get distinct months from the services_weekly table.
+SELECT DISTINCT MONTH
+FROM services_weekly;
+
+### Daily Challenge:
+### Find all unique combinations of service and event type from the services_weekly table 
+### where events are not null or none, along with the count of occurrences for each combination. 
+### Order by count descending.
+SELECT service AS Service, event AS Event_Type, 
+COUNT(*) AS Occurrence_Count
+FROM services_weekly
+WHERE event IS NOT NULL AND event <> 'none'
+GROUP BY service, event
+ORDER BY Occurrence_Count DESC;
